5f9af839cd143762d28b6bfddb61ad6d9b6b7156
keisuke <keisuke7771120@me.com>
2022-03-31 11:34:12 +0900

    ✨ とりあえず内部パッケージを相対パスでインポート（非推奨らしい）

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"./pkg/action"
+	"./pkg/board"
+)
+
+func main() {
+	test := action.RandomAction(4, 5)
+	fmt.Println(test)
+	b := board.Board{}
+	b.Depth = 0
+	b = board.InitBoard(b)
+	fmt.Println(b)
+}
